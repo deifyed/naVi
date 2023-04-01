9d590eff490a8fbc58593c8991222a69f8d80017
Julius Pedersen <julius@tuuturu.io>
2023-04-01 21:18:59 +0200

    👌 Improve plugin API

--- lua/navi/init.lua
+++ lua/navi/init.lua
@@ -2,11 +2,8 @@ local api = require('navi.api')
 
 local M = {}
 
-function M.navi()
-    print("Hey from Navi!")
+function M.navi_open()
+    api.request_without_context()
 end
-
-M.request_without_context = api.request_without_context
-M.request_with_context = api.request_with_context
 
 return M
