edc21e1361d2b65d6523db26baa2b4391fa6337d
Julius Pedersen <julius@tuuturu.io>
2023-04-06 18:56:21 +0200

    👌 Remove redundant prompt parts

--- lua/navi/conversation.lua
+++ lua/navi/conversation.lua
@@ -2,12 +2,9 @@ local M = {}
 
 M.messages = {
     { role = "system", content = "You are an engineer assisting me in writing code." },
-    { role = "user", content = "I will tell you what I need, and you will reply with compilable code." },
     { role = "user", content = "No need to worry about the context, I will provide it." },
-    { role = "user", content = "No natural language. No comments. Pure code only." },
-    { role = "user", content = "Act as though your reply is being written directly into a REPL." },
+    { role = "user", content = "No natural language. No comments. Raw code only." },
     { role = "user", content = "If you are unable to provide an answer, reply with an empty string, i.e.: \"\"." },
-    { role = "user", content = "Any answer you give, should be compilable code, and should not crash the program." },
     { role = "user", content = "The code should be written in a language related to the filetype " .. vim.bo.filetype .. " unless otherwise specified." },
 }
 
