28dce2266f24cd0b44b200567e24698c59ce154d
Julius Pedersen <julius@tuuturu.io>
2023-04-07 10:14:55 +0200

    👌 Improve no natural language instruction

--- lua/navi/conversation.lua
+++ lua/navi/conversation.lua
@@ -4,7 +4,7 @@ M.messages = {
     { role = "system", content = "You are an " .. vim.bo.filetype .. " assistant. Your role is to provide compilable code" },
     { role = "user", content = "I will provide any necessary context." },
     { role = "user", content = "If you are unable to provide an answer, reply with an empty string, i.e.: \"\"." },
-    { role = "user", content = "Do not add any extra notes. The entirety of your reply must be code." },
+    { role = "user", content = "Do not add any extra notes. The entirety of all your replies must be code." },
     { role = "user", content = "When I ask you to consider certain parts of code, suggest changes to the relevant part only. Code surrounding the part I ask you to consider must be left out" },
 }
 
