b15798530a04198fc6008cc563fb070d6f9dda03
Julius Pedersen <julius@tuuturu.io>
2023-04-07 10:55:08 +0200

    👌 Use gd3kr's blender prompt as starting point
    His or her's blender addon seems awesome! Check it out
    [here](https://github.com/gd3kr/BlenderGPT)

--- lua/navi/conversation.lua
+++ lua/navi/conversation.lua
@@ -1,11 +1,58 @@
 local M = {}
 
 M.messages = {
-    { role = "system", content = "You are an " .. vim.bo.filetype .. " assistant. Your role is to provide compilable code" },
-    { role = "user", content = "I will provide any necessary context." },
-    { role = "user", content = "If you are unable to provide an answer, reply with an empty string, i.e.: \"\"." },
-    { role = "user", content = "Do not add any extra notes. The entirety of all your replies must be code." },
-    { role = "user", content = "When I ask you to consider certain parts of code, suggest changes to the relevant part only. Code surrounding the part I ask you to consider must be left out" },
+    { role = "system", content = table.concat({
+        "You are an assistant made for the purpose of helping writing software code.",
+        "- Respond with your answers in markdown (```).",
+        "- Do not respond with anything that is not " .. vim.bo.filetype .. " code.",
+        "- If the user provides code to consider, provide changes to the most recently provided code only.",
+        "\n",
+        "Example:\n",
+        "user: scaffold a function style react component called 'Header'",
+        "assistant: ```",
+        "import React from 'react';",
+        "",
+        "function Header() {",
+        "\treturn (",
+        "\t\t<div>",
+        "\t\t\t{/* your header code here */}",
+        "\t\t</div>",
+        "\t);",
+        "}",
+        "",
+        "export default Header;",
+        "```",
+        "\n",
+        "\n",
+        "Example:\n",
+        "user: consider the following:```\n",
+        "\t\t<div>",
+        "\t\t\t{/* your header code here */}",
+        "\t\t</div>",
+        "```",
+        "user: the div should be a header tag",
+        "assistant: ```",
+        "\t\t<header>",
+        "\t\t\t{/* your header code here */}",
+        "\t\t</header>",
+        "```",
+        "\n",
+        "\n",
+        "Example:\n",
+        "user: consider the following:```\n",
+        "\t\t<header>",
+        "\t\t\t{/* your header code here */}",
+        "\t\t</header>",
+        "```",
+        "user: the header should contain a h1 with the text 'Hello World'",
+        "assistant: ```",
+        "\t\t<header>",
+        "\t\t\t<h1>Hello World</h1>",
+        "\t\t</header>",
+        "```",
+        "\n",
+        "\n",
+    }, "\n") },
 }
 
 function M.push(content)
@@ -18,7 +65,7 @@ function M.push(content)
 end
 
 function M.pushWithContext(context, content)
-    M.push("Consider the following:\n\n```" .. context .. "```\n\n")
+    M.push("Consider the following:```\n" .. context .. "```")
     M.push(content)
 end
 
