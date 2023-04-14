34ca50ec5cc50994b42e1ce36e1963c5fc000fb0
Julius Pedersen <julius@tuuturu.io>
2023-04-14 20:48:56 +0200

    🐛 Fix codeblock extraction

--- lua/navi/utils.lua
+++ lua/navi/utils.lua
@@ -2,6 +2,8 @@ local log = require("navi.log")
 
 local M = {}
 
+local codeblock_delimiter = "```"
+
 function string_split(inputstr, sep)
         if sep == nil then
                 sep = "%s"
@@ -13,20 +15,38 @@ function string_split(inputstr, sep)
         return t
 end
 
-function table_count(t)
-        local count = 0
-        for _ in pairs(t) do count = count + 1 end
-        return count
+function get_codeblock_indices(lines)
+    local start = -1
+    local stop = -1
+
+    for i, line in ipairs(lines) do
+        if line == codeblock_delimiter then
+            if start == -1 then 
+                start = i + 1
+            else 
+                stop = i - 1
+            end
+        end
+    end
+
+    return start, stop
 end
 
 function M.cleanResponse(cfg, response)
     local splitResponse = string_split(response, "\n")
-    local unpackedResponse = {unpack(splitResponse, 2, table_count(splitResponse) - 1)}
+    local start, stop = get_codeblock_indices(splitResponse)
 
-    if cfg.debug then
-        print(vim.inspect(splitResponse))
-        print(vim.inspect(unpackedResponse))
+    log.d("start: " .. start)
+    log.d("stop: " .. stop)
+
+    if cfg.debug and start == stop then
+        return {"error: no code block found. see :messages for more info"}
     end
+
+    local unpackedResponse = {unpack(splitResponse, start, stop)}
+
+    log.d(vim.inspect(splitResponse))
+    log.d(vim.inspect(unpackedResponse))
 
     return unpackedResponse
 end
