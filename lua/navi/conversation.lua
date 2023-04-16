d7aea88988908b25c45e1154128b56e7feb3d707
Julius Pedersen <julius@tuuturu.io>
2023-04-16 15:50:10 +0200

    🐛 Fix formatting with stylua

--- lua/navi/conversation.lua
+++ lua/navi/conversation.lua
@@ -1,37 +1,40 @@
 local M = {}
 
 M.messages = {
-    { role = "system", content = table.concat({
-        "You are an assistant made for the purpose of helping writing " .. vim.bo.filetype .. " code.",
-        "- Always respond with your suggestions surrounded by codeblocks (```).",
-        "- Do not provide explanations or comments.",
-        "\n",
-        "Example:\n",
-        "user: scaffold a function style react component called 'Header'",
-        "assistant: ```",
-        "import React from 'react';",
-        "",
-        "function Header() {",
-        "\treturn (",
-        "\t\t<div>",
-        "\t\t\t{/* your header code here */}",
-        "\t\t</div>",
-        "\t);",
-        "}",
-        "",
-        "export default Header;",
-        "```",
-        "\n",
-        "\n",
-        "user: Consider the following code: ```\t<title>Title</title>```\nthe title should be \"Hello world\"",
-        "assistant: ```\t<title>Hello world</title>```",
-    }, "\n") },
+    {
+        role = "system",
+        content = table.concat({
+            "You are an assistant made for the purpose of helping writing " .. vim.bo.filetype .. " code.",
+            "- Always respond with your suggestions surrounded by codeblocks (```).",
+            "- Do not provide explanations or comments.",
+            "\n",
+            "Example:\n",
+            "user: scaffold a function style react component called 'Header'",
+            "assistant: ```",
+            "import React from 'react';",
+            "",
+            "function Header() {",
+            "\treturn (",
+            "\t\t<div>",
+            "\t\t\t{/* your header code here */}",
+            "\t\t</div>",
+            "\t);",
+            "}",
+            "",
+            "export default Header;",
+            "```",
+            "\n",
+            "\n",
+            'user: Consider the following code: ```\t<title>Title</title>```\nthe title should be "Hello world"',
+            "assistant: ```\t<title>Hello world</title>```",
+        }, "\n"),
+    },
 }
 
 function M.push(content)
     local msg = {
         role = "user",
-        content = content
+        content = content,
     }
 
     table.insert(M.messages, msg)
@@ -52,7 +55,7 @@ end
 function M.pushResponse(content)
     local msg = {
         role = "assistant",
-        content = content
+        content = content,
     }
 
     table.insert(M.messages, msg)
